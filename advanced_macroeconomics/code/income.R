a7f0a5adc1148f661ca70ac2db57d025977c7f02
CommonEconomist <weezel.van@gmail.com>
2016-12-08 17:47:00 +0100

    Update data + code

---
+++ advanced_macroeconomics/code/income.R
@@ -0,0 +1,30 @@
+# Example income over time.
+# Data taken from:
+# http://www.ggdc.net/maddison/maddison-project/home.htm
+par(bty="n",las=1,cex.axis=1.5,cex.lab=1.5)
+
+# Load data and write to vectors
+maddison<-read.csv("maddison.csv",header=TRUE)
+maddison<-maddison[maddison$Year>=1700,] # Subset due to missing values
+
+year<-maddison$Year
+nld<-maddison[,2] # Netherlands
+zaf<-maddison[,3] # South Africa (Cape Colony)
+idn<-maddison[,4] # Indonesia (Java)
+
+
+# Plot data
+plot(year,nld,ylim=c(450,26000),xlim=c(1700,2020),type="b",log="y",
+     xlab="",ylab="",axes=FALSE,pch=0)
+text(1720,nld[1]+250,"the Netherlands",cex=1.2)
+par(new=TRUE)
+plot(year,zaf,ylim=c(450,26000),xlim=c(1700,2020),type="b",log="y",
+     xlab="",ylab="",axes=FALSE,pch=19)
+text(1720,zaf[1]-400,"South Africa \n (Cape Colony)",cex=1.2)
+par(new=TRUE)
+plot(year,idn,ylim=c(450,26000),xlim=c(1700,2020),type="b",log="y",
+     xlab="",ylab="",axes=FALSE,pch=17,col="steelblue4")
+text(1790,idn[20],"Indonesia \n (Java)",cex=1.2)
+axis(1,tick=FALSE);axis(2,tick=FALSE,line=-1)
+
+
