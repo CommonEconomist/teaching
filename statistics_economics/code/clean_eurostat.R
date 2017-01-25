7e90c9136c4913a87dfb346485c41a0ba263acf4
CommonEconomist <weezel.van@gmail.com>
2017-01-25 11:19:10 +0100

    Update code+data Eurostat data

--- statistics_economics/code/clean_eurostat.R
+++ statistics_economics/code/clean_eurostat.R
@@ -74,16 +74,53 @@ dat<-merge(dat,income,all.x=TRUE)
 dat<-merge(dat,doctors,all.x=TRUE)
 
 #------------------------------------------------------------------------------
-#### 7) Create some categorical variables ####
-# - Dummy variable for former Warschau pact countries
+#### 7) Create variables ####
+# - Mortality rate
+# - Infant mortality rate
+# - Female mortality rate
+# - Mortality rate middle aged
+dat$mortality<-dat$deaths/(dat$pop+dat$deaths)*1000
+dat$infant.mortality<-dat$deaths.u5/(dat$pop.u5+dat$deaths.u5)*1000
+dat$female.mortality<-dat$deaths.female/(dat$pop.female+dat$deaths.female)*1000
+dat$middle.aged.mortality<-dat$deaths.middle.aged/
+  (dat$pop.middle.aged+dat$deaths.middle.aged)*1000
+
+#------------------------------------------------------------------------------
+#### 8) Create some categorical variables ####
+# - Dummy variable for former Warsaw pact countries and Soviet states
 # - Dummy for areas that qualify for structural funds
-# - Three income categories
+# - Income groups (4)
+# - Round reached at Euro 2012
+
+# Eastbloc
+wp<-c("BG","CZ","SK","HU","PL","RO","EE","LT","LV")
+dat$eastbloc<-ifelse(dat$CCODE %in% wp,1,0)
+
+# Structural funds
+# Qualifies when GDP is lower than 75% of EU average
+dat$structural.funds<-ifelse(dat$eur.pc< .75*mean(dat$eur.pc,na.rm=TRUE),1,0)
+
+# Income groups
+q<-as.numeric(quantile(dat$eur.pc,probs=c(.25,.5,.75),na.rm=TRUE))
+dat$q1<-ifelse(dat$eur.pc<=q[1],1,0)
+dat$q2<-ifelse(dat$eur.pc>q[1] & dat$eur.pc<=q[2],2,0)
+dat$q3<-ifelse(dat$eur.pc>q[2] & dat$eur.pc<=q[3],3,0)
+dat$q4<-ifelse(dat$eur.pc>q[3],4,0)
+dat$income=dat$q1+dat$q2+dat$q3+dat$q4
+
+# Euro 2012
+euro2012<-data.frame(CCODE=unique(ccode),
+                     euro2012=c(0,0,2,1,3,0,1,2,4,2,1,4,0,0,0,0,
+                             0,0,1,0,1,3,0,0,0,0,1,2,0,0,0,0,
+                             0,0,0,0))
+dat<-merge(dat,euro2012,all.x=TRUE)
 
 
-
-
-
-
-eurostat<-dat
+#------------------------------------------------------------------------------
+#### 9) Subset and save data ####
+eurostat<-dat[,c("GEO","CCODE","mortality","infant.mortality",
+                 "female.mortality","middle.aged.mortality","eur.pc","pps.pc",
+                 "inh.per.doc","income","structural.funds","eastbloc",
+                 "euro2012")]
 
 save(eurostat,file="data/Eurostat.RData")
