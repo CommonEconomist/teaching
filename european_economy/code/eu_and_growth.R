e1b5289a6000c18560e2f827d3cb2a88ab7cfce4
CommonEconomist <weezel.van@gmail.com>
2017-04-20 16:00:50 +0200

    Misc.

--- european_economy/code/eu_and_growth.R
+++ european_economy/code/eu_and_growth.R
@@ -280,6 +280,26 @@ text(1960,.4,"Poland",cex=1.5)
 rm(list=ls());dev.off()
 
 #------------------------------------------------------------------------------
+#### Population projections ####
+d<-read.csv("data_raw/eurostat_population.csv",stringsAsFactors=FALSE)
+pop_total<-ts(d$Value[d$INDIC_DE=="Population on 1 January - total "],
+                      start=c(2015,1),frequency=1)/1000000
+pop_work<-ts(d$Value[d$INDIC_DE!="Population on 1 January - total "],
+                      start=c(2015,1),frequency=1)
+
+# Plot
+par(mar=c(5,5,3,1),bty="n",las=1,cex.axis=1.5,cex.lab=1.5,cex.main=1.7,
+    mfrow=c(2,1))
+
+plot(pop_total,axes=FALSE,xlab="",ylab="",main="Total population (millions)",
+     lwd=2,ylim=c(500,530))
+axis(2,tick=FALSE)
+
+plot(pop_work,axes=FALSE,xlab="",ylab="",ylim=c(50,70),
+     main="Proportion of population aged 15-64",lwd=2)
+axis(2,tick=FALSE);axis(1,tick=FALSE)
+
+#------------------------------------------------------------------------------
 #### Trade new EU member states ####
 
 # Focus on GDP and trade relative to GDP
